e166a79b6751da2bce4ce0014c0f5e8dabaef04f
joheiss <josef.heiss@jovisco.de>
2025-02-03 16:59:11 +0100

    initial

---
+++ src/main/java/com/jovisco/tutorial/webflux/playground/PlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.jovisco.tutorial.webflux.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlaygroundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jovisco/tutorial/webflux/playground/PlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jovisco.tutorial.webflux.playground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaygroundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
